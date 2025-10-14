3f7017b537b222f39a4ba394ec3f4b11243a945d
Tess <kellythe@ohsu.edu>
2025-10-14 12:28:43 -0400

    modified about_me.py

--- python1/about_me.py
+++ python1/about_me.py
@@ -1,2 +1,15 @@
 #!/usr/bin/env python3
 print ("My name: Tess")
+print ("My favorite color")
+print ("My favoroite: hiking")
+print ("My favorite animal: dog")
+d = "dog"
+print (f"My favorite animal: {d}")
+Theresa = "Tess"
+print (f"My name: {Theresa}")
+import sys
+name = sys.argv[1]
+color = sys.argv[2]
+activity = sys.argv[3]
+animal = sys.argv[4]
+print (name, "is", color, "is", activity, "is", animal, "is")
