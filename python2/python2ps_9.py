6878568261e253808446af9e68fb9301985d23f7
Tess <kellythe@ohsu.edu>
2025-10-14 21:12:37 -0400

    adding python2ps_9.py

---
+++ python2/python2ps_9.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+tess = 24
+if tess: 
+        print("Aye Captain")
+else:
+        print("Not True")
+import sys
+variable = int(sys.argv[1])
+print("variable")
+if variable > 0:
+    print("positive")
+    if variable <50:
+        print("less than 50")
+    else: 
+        print("more than 50")
+    if variable %2 == 0:
+        print("even")
+    else: 
+        print("odd")
+    if variable <50 and variable %2 == 0:
+        print("it is an even number smaller than 50")
+    else:
+        print("variable is not an even number smaller than 50")
+    if variable >50: 
+        print("more than 50")
+    else:
+        print("less than 50")
+    if variable >50 and variable %3 == 0:
+        print("it is larger than 50 and divisible by 3")
+    else: print("it is not larger than 50 nad divisible by 3")
+    if variable == 50:
+        print("equals 50")
+elif variable == 0:
+    print ("equals zero")
+else:
+    print("negative")
+
+
+
+
+
+
+
