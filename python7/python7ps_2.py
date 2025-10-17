e80ad1f0b7bdf77972d1e508d9df3dcda4ef8246
Tess <kellythe@ohsu.edu>
2025-10-17 15:41:28 -0400

    adding python7ps_2.py

--- python7/python7ps_2.py
+++ python7/python7ps_2.py
@@ -7,9 +7,9 @@ with open("python_07_nobody.txt","r") as python_07_nobody_read, open("Tess.txt",
         print(Tess)
 
 #for found in re.sub(r'Nobody',r'Tess', line):
-#don't do this because it interprets this as a string and looping over. string goes letter by letterimport re
-with open("python_07_nobody.txt","r") as python_07_nobody_read, open("Tess.txt","w") as Tess: #python can't get the contents of a file until you tell it to fead the file. Store as a variable for less typing? also think this is just the way you do it. 
-    for line in python_07_nobody_read: #for each line (you could type pencil or something and python knows that it still means line.) in python07_nobody
-        Tess = re.sub(r'Nobody',r'Tess', line)
-        print(Tess)
+    #with open("python_07_nobody.txt","r") as python_07_nobody_read, open("Tess.txt","w") as Tess: #python can't get the contents of a file until you tell it to fead the file. Store as a variable for less typing? also think this is just the way you do it. 
+        #for line in python_07_nobody_read: #for each line (you could type pencil or something and python knows that it still means line.) in python07_nobody
+            #Tess = re.sub(r'Nobody',r'Tess', line)
+            #print(Tess)
 
+#not sure what I was doing in second example...rip
