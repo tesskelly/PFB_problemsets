08d70d60352f46df74c16d188ba5a4b2d96152cf
Tess <kellythe@ohsu.edu>
2025-10-17 15:58:32 -0400

    adding python7ps_2.py

--- python7/python7ps_2.py
+++ python7/python7ps_2.py
@@ -3,13 +3,7 @@
 import re
 with open("python_07_nobody.txt","r") as python_07_nobody_read, open("Tess.txt","w") as Tess: #python can't get the contents of a file until you tell it to fead the file. Store as a variable for less typing? also think this is just the way you do it. 
     for line in python_07_nobody_read: #for each line (you could type pencil or something and python knows that it still means line.) in python07_nobody
-        Tess = re.sub(r'Nobody',r'Tess', line)
-        print(Tess)
+        Tess2 = re.sub(r'Nobody',r'Tess', line)
+        Tess.write(f'{Tess2}')
+        print(Tess2)
 
-#for found in re.sub(r'Nobody',r'Tess', line):
-    #with open("python_07_nobody.txt","r") as python_07_nobody_read, open("Tess.txt","w") as Tess: #python can't get the contents of a file until you tell it to fead the file. Store as a variable for less typing? also think this is just the way you do it. 
-        #for line in python_07_nobody_read: #for each line (you could type pencil or something and python knows that it still means line.) in python07_nobody
-            #Tess = re.sub(r'Nobody',r'Tess', line)
-            #print(Tess)
-
-#not sure what I was doing in second example...rip
